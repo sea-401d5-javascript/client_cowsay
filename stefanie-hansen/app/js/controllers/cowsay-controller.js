e4b6813b350534bb8cb1972a0324f5c4a635afe7
Stefanie Hansen <stefuhnee@gmail.com>
2016-06-28 10:35:49 -0700

    cow is talking

--- stefanie-hansen/app/js/controllers/cowsay-controller.js
+++ stefanie-hansen/app/js/controllers/cowsay-controller.js
@@ -1,6 +1,8 @@
 module.exports = function(app) {
   app.controller('CowsayController', function(cowsayService) {
-    this.message = '';
-    this.cowPic = cowsayService;
+    this.cowPic = '';
+    this.cowSay = function(message) {
+      this.cowPic = cowsayService(message);
+    };
   });
 };
--- stefanie-hansen/app/js/services/cowsay-service.js
+++ stefanie-hansen/app/js/services/cowsay-service.js
@@ -2,8 +2,10 @@ const cowsay = require('cowsay-browser');
 
 module.exports = function(app) {
   app.factory('cowsayService', function() {
-    return cowsay.say({
-      text: 'sup'
-    });
+    return function(message) {
+      return cowsay.say({
+        text: message
+      });
+    };
   });
 };
